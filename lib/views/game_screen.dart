e4221dfdf0522e030af391e061a36936a318d986
donaldamadi <donaldamadi15@gmail.com>
2020-12-13 14:19:33 +0100

    Adding DoubleBackToCloseApp to GameScreen

--- lib/views/game_screen.dart
+++ lib/views/game_screen.dart
@@ -6,6 +6,7 @@ import 'package:welcome_page/constants.dart';
 import 'package:welcome_page/services/database.dart';
 import 'package:welcome_page/services/http.dart';
 import 'package:welcome_page/services/word.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 // import 'package:http/http.dart' as http;
 
 class GameScreen extends StatefulWidget {
@@ -69,32 +70,33 @@ class _GameScreenState extends State<GameScreen> {
       messageController.clear();
     }
   }
-  void changeWord(){
-    setState((){
+
+  void changeWord() {
+    setState(() {
       text = getWord();
     });
   }
 
   checkWord() {
     if (messageController.text.isNotEmpty) {
-      if (messageController.text == word){
+      if (messageController.text == word) {
         Map<String, dynamic> messageMap = {
-        'message': messageController.text,
-        'sentBy': Constants.myName,
-        'time': DateTime.now().microsecondsSinceEpoch,
-      };
-      dataBaseMethods.addConversationMessages(widget.chatRoomId, messageMap);
-      print('${Constants.myName} got it');
-      changeWord();
-      messageController.clear();
-      }else{
+          'message': messageController.text,
+          'sentBy': Constants.myName,
+          'time': DateTime.now().microsecondsSinceEpoch,
+        };
+        dataBaseMethods.addConversationMessages(widget.chatRoomId, messageMap);
+        print('${Constants.myName} got it');
+        changeWord();
+        messageController.clear();
+      } else {
         Map<String, dynamic> messageMap = {
-        'message': messageController.text,
-        'sentBy': Constants.myName,
-        'time': DateTime.now().microsecondsSinceEpoch,
-      };
-      dataBaseMethods.addConversationMessages(widget.chatRoomId, messageMap);
-      messageController.clear();
+          'message': messageController.text,
+          'sentBy': Constants.myName,
+          'time': DateTime.now().microsecondsSinceEpoch,
+        };
+        dataBaseMethods.addConversationMessages(widget.chatRoomId, messageMap);
+        messageController.clear();
       }
     }
   }
@@ -129,67 +131,73 @@ class _GameScreenState extends State<GameScreen> {
         automaticallyImplyLeading: false,
         actions: [
           RaisedButton(
-              color: Colors.grey, onPressed: () {
+              color: Colors.grey,
+              onPressed: () {
                 changeWord();
-              }, child: Text('start'))
+              },
+              child: Text('start'))
         ],
       ),
-      body: Background(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 80,
-                color: kSecondaryColor.withOpacity(0.5),
-                child: Row(children: [
-                  Text("The text is "),
-                  FutureBuilder(
-                    future: text,
-                    builder: (BuildContext context,
-                        AsyncSnapshot<dynamic> snapshot) {
-                      if (snapshot.hasData) {
-                        word = snapshot.data;
-                        return Text(
-                          '${snapshot.data}',
-                          // style: GoogleFonts.roboto(
-                          //     color: Colors.white, fontSize: 30)
-                        );
-                      }
-                      return Text('');
-                    },
-                  ),
-                ]),
-              ),
-              gameMessagesList(),
-              Container(
-                alignment: Alignment.bottomCenter,
-                decoration: kMessageContainerDecoration,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: messageController,
-                        decoration: kMessageTextFieldDecoration,
-                      ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        checkWord();
+      body: DoubleBackToCloseApp(
+        snackBar:
+            const SnackBar(content: Text('Press back again to leave game')),
+        child: Background(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: 80,
+                  color: kSecondaryColor.withOpacity(0.5),
+                  child: Row(children: [
+                    Text("The text is "),
+                    FutureBuilder(
+                      future: text,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<dynamic> snapshot) {
+                        if (snapshot.hasData) {
+                          word = snapshot.data;
+                          return Text(
+                            '${snapshot.data}',
+                            // style: GoogleFonts.roboto(
+                            //     color: Colors.white, fontSize: 30)
+                          );
+                        }
+                        return Text('');
                       },
-                      child: Text(
-                        'Send',
-                        style: GoogleFonts.pacifico(
-                            color: kPrimaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    )
-                  ],
+                    ),
+                  ]),
                 ),
-              )
-            ],
+                gameMessagesList(),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: kMessageContainerDecoration,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: messageController,
+                          decoration: kMessageTextFieldDecoration,
+                        ),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          checkWord();
+                        },
+                        child: Text(
+                          'Send',
+                          style: GoogleFonts.pacifico(
+                              color: kPrimaryColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
